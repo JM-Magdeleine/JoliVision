07cb8a43d52e448a7dbe0dabf9d9db0e7a0e1c59
Julien Marie-Magdeleine <julien.p.mmagdeleine@gmail.com>
2024-07-08 17:07:24 +0200

    primary image embed extraction implementation

--- test.py
+++ test.py
@@ -141,13 +141,105 @@ projector = CPMQwenProjector(input_size=504, output_size=5)
 
 
 
-def get_image_embeds(input_str: str, image: str) -> torch.Tensor:
+def get_image_embeds(model, input_str: str, image: str) -> torch.Tensor:
     # format input just like model.chat
-    # do the same as in generate, but stop when image_bound and input_embeds are there
+    image = img2base64(image)
+    msgs = [{'role': 'user', 'content': input_str}]
+    msgs = json.loads(msgs)
+    
+    # msgs to prompt
+    prompt = ""
+    for i, msg in enumerate(msgs):
+        role = msg["role"]
+        content = msg["content"]
+        assert role in ["user", "assistant"]
+        if i == 0:
+            if image is None:
+                images = []
+            else:
+                assert role == "user", "The role of first msg should be user"
+                if self.config.slice_mode:
+                    images, final_placeholder = self.get_slice_image_placeholder(
+                        image, tokenizer
+                    )
+                    content = final_placeholder + "\n" + content
+                else:
+                    images = [image]
+                    content = (
+                        tokenizer.im_start
+                        + tokenizer.unk_token * self.config.query_num
+                        + tokenizer.im_end
+                        + "\n"
+                        + content
+                    )
+        prompt += "<用户>" if role == "user" else "<AI>"
+        prompt += content
+    prompt += "<AI>"
+    final_input = prompt
+
+    if sampling:
+        generation_config = {
+            "top_p": 0.8,
+            "top_k": 100,
+            "temperature": 0.7,
+            "do_sample": True,
+            "repetition_penalty": 1.05
+        }
+    else:
+        generation_config = {
+            "num_beams": 3,
+            "repetition_penalty": 1.2,
+        }
+
+    generation_config.update(
+        (k, kwargs[k]) for k in generation_config.keys() & kwargs.keys()
+    )
+        
+    # do the same as in generate, but stop when image_bound and input_embeds are loaded
+    # MAX_INP_LENGTH ?
+    # TOKENIZER ?
+    # MAX_NEW_TOKENS ?
+
+    data_list = [final_input]
+    img_list = [images]
+    
+    bs = len(data_list)
+    if img_list == None:
+        img_list = [[] for i in range(bs)]
+    assert bs == len(img_list)
+
+    model_inputs = model._process_list(tokenizer, data_list, max_inp_length)
+    
+    if vision_hidden_states is None:
+        pixel_values = []
+        for i in range(bs):
+            img_inps = []
+            for img in img_list[i]:
+                img_inps.append(model.transform(img).to(model.device))
+            if img_inps:
+                pixel_values.append(img_inps)
+            else:
+                pixel_values.append([])
+        model_inputs["pixel_values"] = pixel_values
+    else:
+        model_inputs["vision_hidden_states"] = vision_hidden_states
+
+    with torch.inference_mode():
+        (
+            model_inputs["inputs_embeds"],
+            vision_hidden_states,
+        ) = model.get_vllm_embedding(model_inputs)
+
+        result = model._decode(model_inputs["inputs_embeds"], tokenizer, **kwargs)
     # recuperate model_inputs
+    image_bound, input_embeds = model_inputs["image_bound"], model_inputs["inputs_embeds"]
 
     # segment out image embeds
-    # return cut-out image embeds    
+    image_embeds = []
+    image_embeds.append(inputs_embeds[image_bound[0], image_bound[-1]])
+    
+    # return cut-out image embeds
+    return image_embeds
 
 EOT_TOKEN_ID=151644
 def tokenize_mixed_modal(input_str: str, image: str) -> List[int]:
